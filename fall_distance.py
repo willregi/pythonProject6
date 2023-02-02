60d17fc50ce6aca7feb6242d03c02ce83f55b8a6
willregi <willregi@oregonstate.edu>
2023-02-01 22:00:40 -1000

    Project 4a

--- fall_distance.py
+++ fall_distance.py
@@ -5,5 +5,5 @@
 def fall_distance(fall_time):
     return 0.5 * 9.8 * fall_time * fall_time
     #"""Determine the distance an object falls due to gravity in a specific time period."""
-time = float(input("Enter the amount of time for which the object is falling:"))
-print("Distance the object has fallen is", fall_distance(time))
+time = float(input("Fall time:"))
+print("dist", fall_distance(time))
